2f3f1a31bcd19d7ba4a4193de7522eda4f71e6d6
harshal-0905 <159798369+harshal-0905@users.noreply.github.com>
2024-02-18 04:24:59 +0530

    Update login_page.dart

--- login_page.dart
+++ login_page.dart
@@ -18,6 +18,7 @@ class LoginPage extends StatelessWidget {
         body: SafeArea(
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(
                   height: 50,
