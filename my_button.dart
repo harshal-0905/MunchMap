720f080730a296868afb978314dee85a86a2b02a
harshal-0905 <159798369+harshal-0905@users.noreply.github.com>
2024-03-02 23:00:38 +0530

    Update my_button.dart

--- my_button.dart
+++ my_button.dart
@@ -3,28 +3,32 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final Function()? onTap;
+  final String text;
+
+  const MyButton({super.key, required this.onTap, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    // ignore: avoid_unnecessary_containers
-    return Container(
-      padding: EdgeInsets.all(22),
-      margin: EdgeInsets.symmetric(horizontal: 25),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 121, 98, 250),
-        borderRadius: BorderRadius.circular(16)
-      ),
-
-      child: const Center(
-        child: Text(
-          "Sign In",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 15
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
           ),
-        )
+        ),
       ),
     );
   }
