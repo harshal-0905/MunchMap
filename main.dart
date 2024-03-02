72f65e0b03fbeaafb49e507dfa27bb96f6e44deb
harshal-0905 <159798369+harshal-0905@users.noreply.github.com>
2024-03-02 23:24:00 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthPage(),
+    );
+  }
+}
