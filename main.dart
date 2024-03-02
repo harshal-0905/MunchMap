81c3332392358bff52bc556dad73181690389c95
harshal-0905 <159798369+harshal-0905@users.noreply.github.com>
2024-03-02 23:26:48 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:login_page/pages/auth_page.dart';
+import 'firebase_options.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:login_page/pages/home.dart';
+import 'pages/login_page.dart';
+import 'pages/signup_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
