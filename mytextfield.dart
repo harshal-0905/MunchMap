d19e0fe0c60e775bfb65d6bd4e0c2e920dffb65f
harshal-0905 <159798369+harshal-0905@users.noreply.github.com>
2024-03-02 23:01:15 +0530

    Update mytextfield.dart

--- mytextfield.dart
+++ mytextfield.dart
@@ -1,37 +1,35 @@
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  // ignore: prefer_typing_uninitialized_variables
-  //final controller;
+  final controller;
   final String hintText;
   final bool obscureText;
 
-   const MyTextField(
-      {super.key,
-      //this.controller,
-      required this.hintText,
-      required this.obscureText});
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextField(
-        //controller: controller,
+        controller: controller,
         obscureText: obscureText,
         decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(color: Colors.white),
-            borderRadius: BorderRadius.circular(16)
-          ),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey),
-          ),
-          fillColor: const Color.fromRGBO(245, 245, 245, 1),
-          filled: true,
-          hintText: hintText,
-          hintStyle: TextStyle(color: Colors.grey[400]),
-        ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey[500])),
       ),
     );
   }
