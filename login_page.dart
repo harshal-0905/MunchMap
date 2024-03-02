20ca2975eebc8522202189c02b36d42d3ca18a0f
harshal-0905 <159798369+harshal-0905@users.noreply.github.com>
2024-03-02 23:03:04 +0530

    Update login_page.dart

--- login_page.dart
+++ login_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:login_page/Components/my_button.dart';
 import 'package:login_page/Components/mytextfield.dart';
@@ -167,4 +168,3 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-
