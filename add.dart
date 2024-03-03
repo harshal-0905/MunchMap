e9102ed2ead77a9a975808d7119c07289e0bb8a6
Ayush Patel <138257486+Ayushh-patel@users.noreply.github.com>
2024-03-04 04:39:17 +0530

    Add files via upload

--- add.dart
+++ add.dart
@@ -11,9 +11,67 @@ class _AddState extends State<Add> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Add Posts/Videos'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_rounded),
+            onPressed: () {},
+          ),
+          title: const Text('Photos / Videos'),
+          centerTitle: false,
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Post",
+                style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: TextField(
+                    //controller: _descriptionController,
+                    decoration: const InputDecoration(
+                        hintText: "Write a caption...",
+                        border: InputBorder.none),
+                    maxLines: 8,
+                  ),
+                ),
+                SizedBox(
+                  height: 45.0,
+                  width: 45.0,
+                  child: AspectRatio(
+                    aspectRatio: 487 / 451,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        fit: BoxFit.fill,
+                        alignment: FractionalOffset.topCenter,
+                        image: NetworkImage(
+                            'https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                      )),
+                    ),
+                  ),
+                ),
+                const Divider(),
+              ],
+            )
+          ],
+        ));
   }
 }
