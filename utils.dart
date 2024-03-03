fc7def2bab846b39e5040d11bd1b681a77197481
Ayush Patel <138257486+Ayushh-patel@users.noreply.github.com>
2024-03-04 04:44:54 +0530

    Update utils.dart

--- utils.dart
+++ utils.dart
@@ -11,3 +11,10 @@ pickImage(ImageSource source) async{
     print('No image is selected');
   }
 }
+showSnackBar(BuildContext context, String text) {
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(text),
+    ),
+  );
+}
