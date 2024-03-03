61d45659a1afdeade439cee4d1eea6c15d5a7bee
Ayush Patel <138257486+Ayushh-patel@users.noreply.github.com>
2024-03-04 04:42:59 +0530

    Update add.dart

--- add.dart
+++ add.dart
@@ -1,4 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:munchmap_org/components/user_provider.dart';
+import 'package:munchmap_org/components/utils.dart';
 
 class Add extends StatefulWidget {
   const Add({super.key});
@@ -9,19 +14,134 @@ class Add extends StatefulWidget {
 
 class _AddState extends State<Add> {
   @override
+
+Uint8List? _file;
+  bool isLoading = false;
+  final TextEditingController _descriptionController = TextEditingController();
+
+  _selectImage(BuildContext parentContext) async {
+    return showDialog(
+      context: parentContext,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Create a Post'),
+          children: <Widget>[
+            SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take a photo'),
+                onPressed: () async {
+                  Navigator.pop(context);
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                }),
+            SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Choose from Gallery'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                }),
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20),
+              child: const Text("Cancel"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+void postImage(String uid, String username, String profImage) async {
+    setState(() {
+      isLoading = true;
+    });
+    // start the loading
+    try {
+      // upload to storage and db
+      String res = await FireStoreMethods().uploadPost(
+        _descriptionController.text,
+        _file!,
+        uid,
+        username,
+        profImage,
+      );
+      if (res == "success") {
+        setState(() {
+          isLoading = false;
+        });
+        if (context.mounted) {
+          showSnackBar(
+            context,
+            'Posted!',
+          );
+        }
+        clearImage();
+         } else {
+        if (context.mounted) {
+          showSnackBar(context, res);
+        }
+      }
+    } catch (err) {
+      setState(() {
+        isLoading = false;
+      });
+      showSnackBar(
+        context,
+        err.toString(),
+      );
+    }
+  }
+
+  void clearImage() {
+    setState(() {
+      _file = null;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _descriptionController.dispose();
+  }
+
+
+
+  
   Widget build(BuildContext context) {
-    return Scaffold(
+    final UserProvider userProvider = Provider.of<UserProvider>(context);
+
+    return _file == null
+        ? Center(
+            child: IconButton(
+              icon: const Icon(
+                Icons.upload,
+              ),
+              onPressed: () => _selectImage(context),
+            ),
+          )
+    :Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
           leading: IconButton(
             icon: const Icon(Icons.arrow_back_rounded),
-            onPressed: () {},
+            onPressed: clearImage,
           ),
           title: const Text('Photos / Videos'),
           centerTitle: false,
-          actions: [
+          actions: <Widget>[
             TextButton(
-              onPressed: () {},
+              onPressed: () => postImage(
+                 userProvider.getUser.uid,
+                    userProvider.getUser.username,
+                    userProvider.getUser.photoUrl,
+              ),
               child: const Text(
                 "Post",
                 style: TextStyle(
@@ -33,19 +153,23 @@ class _AddState extends State<Add> {
           ],
         ),
         body: Column(
-          children: [
+          children: <Widget>[
+            isLoading
+                    ? const LinearProgressIndicator()
+                    : const Padding(padding: EdgeInsets.only(top: 0.0)),
+                const Divider(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+              children: <Widget> [ 
                 CircleAvatar(
                   backgroundImage: NetworkImage(
-                      'https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                      /*'https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'*/ userProvider.getUser.photoUrl,),
                 ),
                 SizedBox(
                   width: MediaQuery.of(context).size.width * 0.4,
                   child: TextField(
-                    //controller: _descriptionController,
+                    controller: _descriptionController,
                     decoration: const InputDecoration(
                         hintText: "Write a caption...",
                         border: InputBorder.none),
@@ -62,8 +186,9 @@ class _AddState extends State<Add> {
                           image: DecorationImage(
                         fit: BoxFit.fill,
                         alignment: FractionalOffset.topCenter,
-                        image: NetworkImage(
-                            'https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                        image: MemoryImage(_file!),
+                      /*  image: NetworkImage(
+                            'https://images.pexels.com/photos/235986/pexels-photo-235986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),*/
                       )),
                     ),
                   ),
