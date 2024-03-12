299a3c57de7654a6a96cebb8c65685cb67ba3334
Kavya jain <60062468+kavyajain123@users.noreply.github.com>
2024-03-12 11:23:39 +0530

    Add files via upload

--- notification.dart
+++ notification.dart
@@ -1,18 +1,78 @@
 import 'package:flutter/material.dart';
 
-class Notifications extends StatefulWidget {
-  const Notifications({super.key});
+class NotitcationTap extends StatelessWidget {
+  NotitcationTap({Key? key}) : super(key: key);
+  List newItem = ["liked", "follow"];
+  List todayItem = ["follow", "liked", "liked"];
 
-  @override
-  State<Notifications> createState() => _NotificationState();
-}
+  List oldesItem = ["follow", "follow", "liked", "liked"];
 
-class _NotificationState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('notification'),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "New",
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: newItem.length,
+                  itemBuilder: (context, index) {
+                    return newItem[index] == "follow"
+                        ? CustomFollowNotifcation()
+                        : CustomLikedNotifcation();
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Today",
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                ),
+                ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: todayItem.length,
+                  itemBuilder: (context, index) {
+                    return todayItem[index] == "follow"
+                        ? CustomFollowNotifcation()
+                        : CustomLikedNotifcation();
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Oldest",
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                ),
+                ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: oldesItem.length,
+                  itemBuilder: (context, index) {
+                    return oldesItem[index] == "follow"
+                        ? CustomFollowNotifcation()
+                        : CustomLikedNotifcation();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
